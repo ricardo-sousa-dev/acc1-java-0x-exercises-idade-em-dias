58c41a5986a39b92c972a78294f6e09749ae2833
ricardo-sousa <rwmsousa@gmail.com>
2022-03-18 19:00:34 -0300

    challenge resolution

--- src/main/java/com/trybe/idadeemdias/Pessoa.java
+++ src/main/java/com/trybe/idadeemdias/Pessoa.java
@@ -1,7 +1,10 @@
 package com.trybe.idadeemdias;
 
 public class Pessoa {
-	
-
-	
+  /**
+  * Método inicial.
+  */
+  public int calcularIdadeEmDias(int anos, int meses, int dias) {
+   return (anos * 365) + (meses * 30) + dias;
+  }
 }
